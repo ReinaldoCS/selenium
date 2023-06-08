5df4c75bdc744049c19b44994230e4ef8533c58a
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2023-06-08 13:09:48 -0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // habilitar para executar sem abrir o browser
+        options.addArguments("--no-sandbox"); // habilitar para executar sem abrir o browser
+        options.addArguments("--start-maximized"); // habilitar para executar sem abrir o browser
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+
+        driver.get("https://www.google.com");
+
+
+
+        driver.findElement(By.id("APjFqb")).sendKeys("github");
+        driver.findElement(By.xpath("//input[@name='btnK']")).click();
+
+        String text = driver.findElement(By.id("result-stats")).getText();
+        System.out.println(text);
+
+        System.out.println("fim");
+    }
+
+
+}
