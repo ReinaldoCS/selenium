d1de9ce48df8d4547dbf977a042981bfa180467b
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2023-06-12 16:26:43 -0300

    teste v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,22 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
         WebDriverManager.chromedriver().setup();
 
@@ -65,7 +69,21 @@ public class Main {
         driver.get("https://www.google.com");
 
         driver.findElement(By.id("APjFqb")).sendKeys("github");
-        driver.findElement(By.xpath("//input[@name='btnK']")).click();
+
+        By button = By.xpath("//input[@name='btnK']");
+
+        try {
+            WebDriverWait EsperaJS = new WebDriverWait(driver, Duration.ofSeconds(30));
+            EsperaJS.until(ExpectedConditions.presenceOfElementLocated(button));
+            JavascriptExecutor jse = (JavascriptExecutor) driver;
+            jse.executeScript("arguments[0].click();", driver.findElement(button));
+        } catch (Exception e) {
+            String mensagemErro = "TOA - Erro ao clicar no elemento: '"+button+"', tempo de espera: "+30+"s" + e.toString();
+
+            System.err.println(mensagemErro);
+            throw new Exception(mensagemErro);
+        }
+
         String text = driver.findElement(By.id("result-stats")).getText();
 
         System.out.println(text);
